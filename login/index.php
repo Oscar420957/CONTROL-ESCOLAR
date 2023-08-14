0ca05c152c49464be8adbb87e85887c5e7f3b62d
Gama19999 <gama.rios0110@gmail.com>
2023-08-14 15:59:03 -0600

    carpetas

---
+++ login/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Inicio de Sesión</title>
+</head>
+<body>
+	<form method="post" action="../inicio/">
+		<input type="text" name="user" id="user">
+		<input type="password" name="password" id="password">
+	</form>
+</body>
+</html>
