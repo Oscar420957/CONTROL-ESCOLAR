ca21a7dfedf4ae12335d4e966649032b77490d71
Gama19999 <gama.rios0110@gmail.com>
2023-08-15 20:16:42 -0600

    errores tecnicos

--- login/check.php
+++ login/check.php
@@ -8,11 +8,11 @@ $pass = $_POST['password'];
 $conn = mysqli_connect("74.208.191.226","gamanto","Serial3/0","ceumh");
 
 
-$query = "select pswd from acceso_alumno where id_Alumno = $user";
+$query = "select password from acceso_alumno where id_Alumno = $user";
 $result = $conn->query($query);
 
 while ($field = $result->fetch_object()) {
-	if ($field->pswd == $pass) {
+	if ($field->password == $pass) {
 		$_SESSION['usersession'] = $user;
 		echo json_encode(array("val" => "pass"));
 	} else {
