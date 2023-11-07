a96362cccf3e39b6371b54263faeae53582b27c5
Gama19999 <gama.rios0110@gmail.com>
2023-11-06 21:36:58 -0600

    Event para el password

--- login/index.js
+++ login/index.js
@@ -1,39 +1,48 @@
-$(document).ready(
-	$("#enter").click(function() {
-		if ($("#user").val() == "" || ($("#password").val() == ""))
-		{
-			alert("No puede haber campos vacios!");
+$(document).ready(function() {
+	let password = document.getElementById("password");
+	password.addEventListener("keypress", function(event) {
+		if (event.key === "Enter") {
+			event.preventDefault();
+			proceso();
 		}
-		let form = $("#form-log"); // Obtiene el objeto form 
-		let datos = form.serialize(); // Serializa los datos del form
+	});
+	$("#enter").on("click", () => {proceso();});
+});
 
-		console.log(datos);
+function proceso() {
+	if ($("#user").val() == "" || ($("#password").val() == ""))
+	{
+		alert("No puede haber campos vacios!");
+	}
+	let form = $("#form-log"); // Obtiene el objeto form 
+	let datos = form.serialize(); // Serializa los datos del form
 
-		// Prepara la peticion ajax
-		let ajax = $.ajax({
-			url: "check.php", // Lee la url de action del form
-			method: form.prop("method"), // Lee el metodo a usar del form
-			data: datos, // Variable con los datos serializados
-			dataType: "json" // Tipo de datos que le pasamos en data
-		});
+	console.log(datos);
 
-		// Esta parte se ejecuta si no hay error en la peticion
-		ajax.done(function(respuesta) {
-			if (respuesta.val == "pass") {
-				form.attr("action", `../inicio/${respuesta.inicio}/`);
-				form.submit();
-			} else if (respuesta.val == "notfound") {
-				alert("Datos erroneos!");
-			}  else {
-				alert("Datos erroneos!");
-			}
-		});
+	// Prepara la peticion ajax
+	let ajax = $.ajax({
+		url: "check.php", // Lee la url de action del form
+		method: form.prop("method"), // Lee el metodo a usar del form
+		data: datos, // Variable con los datos serializados
+		dataType: "json" // Tipo de datos que le pasamos en data
+	});
 
-		// Esta parte se ejecuta si hay un error
-		ajax.fail(function(jqXHR, status) {
-			/*console.log(jqXHR)
-			console.log(status)
-			console.log(status.responseText);*/
-		});
-	})
-);
+	// Esta parte se ejecuta si no hay error en la peticion
+	ajax.done(function(respuesta) {
+		if (respuesta.val == "pass") {
+			form.attr("action", `../inicio/${respuesta.inicio}/`);
+			form.submit();
+		} else if (respuesta.val == "notfound") {
+			alert("Datos erroneos!");
+		}  else {
+			alert("Datos erroneos!");
+		}
+	});
+
+	// Esta parte se ejecuta si hay un error
+	ajax.fail(function(jqXHR, status) {
+		/*console.log(jqXHR)
+		console.log(status)
+		console.log(status.responseText);*/
+	});
+}
