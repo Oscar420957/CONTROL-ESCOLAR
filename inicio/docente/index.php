309e8b2b6e1c0ce4ba02c64023f7b737045fbbd5
Oscar420957 <mo420957@uaeh.edu.mx>
2023-11-07 23:11:55 -0600

    Apartdo Docente Comienzo

---
+++ inicio/docente/index.php
@@ -0,0 +1,61 @@
+<?php 
+	ini_set('session.gc_maxlifetime', 60); 
+	ini_set('session.cookie_maxlifetime', 60); 
+
+	session_start();
+
+	if (!isset($_SESSION['usersession'])) {
+		header('Location: ../../');
+	}
+
+	$user = $_SESSION['usersession'];
+
+	#SELECT DATOS DE DOCENTE
+	$conn = mysqli_connect("74.208.191.226","gamanto","Serial3/0","ceumh");
+
+	$query = "select d.id_docente, d.nombre as nomDoc, d.apellido_Pat, d.apellido_Mat, m.id_materia, m.nombre, m.nivel_educativo, c.nombre, m.cuatrimestre, m.creditos from docentes as d, materia as m, carrera as c, materia_docente as md where md.id_docente = d.id_docente and md.id_materia = m.id_materia and m.carrera = c.id_carrera and d.id_docente = ".$user;
+
+	$result = $conn->query($query);
+
+	while ($row = $result->fetch_object()) {
+		$nom_Doc = $row->nomDoc;
+		$ape_Pat = $row->apellido_Pat;
+		$ape_Mat = $row->apellido_Mat;
+	}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<script src="https://kit.fontawesome.com/b32a76d93a.js" crossorigin="anonymous"></script>
+	<link rel="stylesheet" type="text/css" href="css.css">
+	<script type="text/javascript" src="../../jquery/code.jquery.com_jquery-3.7.0.min.js"></script>
+	<title></title>
+</head>
+<body>
+	<div id="container">
+		<nav id="menu">
+			<ul>
+				<li><i class="fa-solid fa-house" id="inicio"></i></li>
+				<li><i class="fa-solid fa-book" id="califs"></i></li>
+				<li><i class="fa-solid fa-check" id="lista"></i></li>
+				<li><i class="fa-solid fa-clock" id="horario"></i></li>
+				<li><i class="fas fa-sign-out-alt" id="salir"></i></li>
+			</ul>
+		</nav>
+
+		<div id="v-inicio">
+			<div id="img-alu" style="background: url('./img/<?php echo $user.".jpg" ?>') no-repeat; background-size: cover;"></div>
+			<div id="nom-alu">
+				<div id="border-top"></div>
+				<div id="datos">
+					Bienvenido de nuevo: <?php echo $nom_Doc." ".$ape_Pat." ".$ape_Mat ?><br>
+				</div>
+			</div>
+		</div>
+
+	</div>
+	<script type="text/javascript" src="js.js"></script>
+</body>
+</html>
---
+++ inicio/docente/js.js
@@ -0,0 +1,57 @@
+$(document).ready(() => {
+	$("#inicio").on("click", () => {
+		offAll();
+		showInicio();
+	});
+	$("#califs").on("click", () => {
+		offAll();
+		showCalifs();
+	});
+	$("#lista").on("click", () => {
+		offAll();
+		showLista();
+	});
+	$("#horario").on("click", () => {
+		offAll();
+		showHorario();
+	});
+	$("#salir").on("click", () => window.location.assign("./phps/closeSession.php"));
+});
+
+let showInicio = () => {
+	$("#v-inicio").css("display", "grid");
+}
+let showCalifs = () => {
+	$("#v-califs").css("display", "grid");
+	/*let usuario = $("#user").val();
+	$.ajax({
+		url: "consultaCalifs.php",
+		method: "post",
+		data: {"user": usuario}, 
+		dataType: "json"
+	});
+	ajax.done(function(respuesta)
+		{
+			let acor = $("#acordeon");
+			for(let i in respuesta)
+			{
+				let contenido = "<div class="accordion-item"><div class="accordion-header">Título 1</div><div id="parciales"><div class="accordion-content" id="FP">Primer parcial</div><div class="accordion-content" id="SP">Segundo parcial</div><div class="accordion-content" id="TP">Tercer parcial</div></div>";
+				let div = $("<div> </div>").
+			}
+		});
+	console.log(user);*/
+}
+let showLista = () => {
+	$("#v-lista").css("display", "grid");
+}
+let showHorario = () => {
+	$("#v-horario").css("display", "grid");
+}
+
+let offAll = () => {
+	let divs = ["#v-inicio","#v-califs","#v-lista","#v-horario"];
+	for (i of divs) {
+		$(i).removeClass();
+		$(i).css("display", "none");
+	}
+}
